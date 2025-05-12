2025cf730171b026f69b5c8289031071bd387614
HelioM2 <hantunes059@gmail.com>
2025-05-12 21:01:58 +0100

    update URL server do frontend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api', // muda para tua URL/backend
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  baseURL: 'https://ecommercebackend-backend-afropoderosa.up.railway.app',
   withCredentials: true, // se usares cookies para sessão
 });
 
--- src/services/apiProdutos.js
+++ src/services/apiProdutos.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiProdutos = axios.create({
-  baseURL: 'http://localhost:5000/api/product', // URL do backend para produtos
+  baseURL: 'https://ecommercebackend-backend-afropoderosa.up.railway.app', // URL do backend para produtos
   withCredentials: true, // se usares cookies para sessão
 });
 
